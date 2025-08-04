290c9c52ee0d2f7a702698d89953364b1c948d26
ramaro <ricardoamaro@lithia.com>
2025-08-04 17:41:31 -0300

    first commit

---
+++ src/main/java/br/com/fiap/hackaton/HackatonFiapFase5Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonFiapFase5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonFiapFase5Application.class, args);
+	}
+
+}
